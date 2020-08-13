b8e239d19dfe3f9aa2e50dfc6ef1337bf53284f1
Jomskie <jbagalanon@gmail.com>
2020-08-13 22:19:25 +0800

    Added Menu Reservation and Submenu Model

---
+++ Expresso/Expresso/Models/Reservation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Expresso.Models
+{
+    public class Reservation
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string TotalPeople { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime Time { get; set; }
+
+    }
+}
