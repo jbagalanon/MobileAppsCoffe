8027a1252e11230334f5d639232d57d90b248c6d
Joemar <jbagalanon@gmail.com>
2020-09-01 07:36:25 +0800

    Submennu Code Behind Added

--- Expresso/Expresso/Pages/SubMenuPage.xaml.cs
+++ Expresso/Expresso/Pages/SubMenuPage.xaml.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Expresso.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Menu = Expresso.Models.Menu;
@@ -12,10 +13,20 @@ namespace Expresso.Pages
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class SubMenuPage : ContentPage
-	{
+    {
+        public ObservableCollection<SubMenu> SubMenus;
+
 		public SubMenuPage (Menu menu)
 		{
 			InitializeComponent ();
+
+            SubMenus = new ObservableCollection<SubMenu>();
+
+            foreach (var submenu in menu.SubMenus)
+            {
+                SubMenus.Add(submenu);
+            }
+            LvSubMenu.ItemsSource = SubMenus;
 		}
 	}
 }
